d55229502545e4032d32ad81450399e68de6f88e
A T <toegelanna@outlook.com>
2019-09-14 12:29:13 +0200

    trainer done

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -25,25 +25,30 @@ function createServer() {
         response.write(JSON.stringify({ state: state }));
         break;
       case '/add':
-        state = state + 1
+        state = state + 1;
         response.writeHead(200, { 'Content-Type': 'application/json' });
         response.write(JSON.stringify({ state: state }));
         break;
       case '/subtract':
-        state = state - 1
+        state = state - 1;
         response.writeHead(200, { 'Content-Type': 'application/json' });
         response.write(JSON.stringify({ state: state }));
         break;
       case '/reset':
-        state = 10
         response.writeHead(200, { 'Content-Type': 'application/json' });
         response.write(JSON.stringify({ state: state }));
         break;
 
       default:
+        state = 10;
         response.writeHead(404, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ error: 'Not Found' }));
+        response.write(JSON.stringify({ error: 'Not found' }));
         break;
+
+      // default:
+      //   response.writeHead(200, { 'Content-Type': 'application/json' });
+      //   response.write(JSON.stringify({ state: state }));
+      //   break;
 
     }
 

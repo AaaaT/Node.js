8392a830f7c159ad156abc51af9d306eb8c1bb9f
A T <toegelanna@outlook.com>
2019-09-18 14:02:10 +0200

    trainer for fs. reading and writing

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,7 +1,18 @@
 'use strict';
 
 // TODO: Write the homework code in this file
-const fs = require ('fs');
+const fs = require('fs');
 
-const readHelp = fs.readFileSync('src/help.txt', 'utf8');
-console.log(readHelp);
+function showHelp() {
+  fs.readFile('src/help.txt', 'utf8', function(err, data) {
+    if (err) {
+      console.log('Error: cannot read the Help file');
+    }
+    else {
+      fs.writeFile('src/writeHelp.txt', data); 
+    }
+    console.log(data);
+  });
+}
+
+console.log(showHelp);

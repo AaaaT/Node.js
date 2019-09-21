03c92d88490e70a131fbae0b62b7df01d5e1ce49
A T <toegelanna@outlook.com>
2019-09-21 22:06:52 +0200

    almost done with the trial trainer excercise

--- MY-Node_Trainer/first-app/app.js
+++ MY-Node_Trainer/first-app/app.js
@@ -1,14 +1,77 @@
+//------------------Events (the same with an Arrow function)
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+// Register a listener
+emitter.on('messageLogged', (arg) => {
+   console.log('Listener called', arg);
+})
+// Raise an event
+emitter.emit('messageLogged',  {id:1, url: 'http://'});
+
+// Raise: logging (data: message) 
+//My excercise
+emitter.on('Hej logged', (message) => {
+   console.log('Hej called', message);
+});
+emitter.emit('Hej logged', {id:'Hej-hej'} );
+
+
+
+
+//   //------------------Events / Event Arguments
+// const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+// // Register a listener
+// emitter.on('messageLogged', function(arg){ //or e, eventArg
+//    console.log('Listener called', arg);
+// })
+// //emitter.addListener is the same as emitter.on
+
+// // Raise an event
+// emitter.emit('messageLogged',  {id:1, url: 'http://'});
+// // making a noise, produce - signal that an event has happened
+
+
+
+
+//   //-----------------File System Module
+// const fs = require('fs');
+
+// // const files = fs.readdirSync('./');
+// // console.log(files);
+
+//          // put some character instead of './' to throw and check an error
+// fs.readdir('./', function(err, files){
+//    if (err) console.log('Error', err);
+//    else console.log('Result:', files);
+// })
+
+  
+//   //-----------------Information about my operating system - OS
+// const os = require('os');
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+
+// console.log('Total Memory: ' + totalMemory);
+// console.log('Free Memory:  ' + freeMemory);
+
+//   //------------Path
+// const path = require('path');
+// var pathObj = path.parse(__filename);
+// console.log(pathObj);
+
+
+
+//   //-------------------Exporting Functions and Objects
 // console.log(module);
-
-const log = require('./logger.js')
-// console.log(log);
-
-//log.log(); //message - from exported object
-   //or
-log(); //message - from exported function
+// const log = require('./logger.js')
+// // console.log(log);
+// //log.log(); //message - from exported object
+//    //or
+// log(); //message - from exported function
 
 
-
+//   //--------------------Global Scope in Node.js
 // function sayHello(name){
 //     console.log('Hello ' + name);
 // }

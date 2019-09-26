5f17227920bf93e894346c5af32e1e182bb6ac09
unknown <toegelanna@outlook.com>
2019-09-26 20:13:43 +0200

    j

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -25,6 +25,9 @@ app.get('/todos/:id', (req, res) =>{
 // app.post-createTodo
 // // markAsDone (POST /todos/:id/done)
 // // Sets the done flag of a single to-do to true
+app.post('/todos/:id/done', (req, res) =>{
+    
+})
 
 // app.delete-deleteTodo
 // // markAsNotDone (DELETE /todos/:id/done)

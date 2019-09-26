f45d2ef4e2346ce031bb5bbb34d4980592a9fb5d
unknown <toegelanna@outlook.com>
2019-09-26 18:02:31 +0200

    AaaaT week1 homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -2,22 +2,12 @@
 
 const http = require('http');
 
-/* `createServer` MUST return an instance of `http.Server` otherwise the tests
- * will fail.
- */
 function createServer() {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
 
-    // response.write('Hello Anna!');
-  
-    // const dataStructure = {'greeting':'hello you'};
-    // const text = JSON.stringify(dataStructure);
-    // response.write(text);
     console.log(request.method, request.url); //e.g. GET /10
-    // console.log('hello world');
 
     switch (request.url) {
       case '/state':
@@ -44,12 +34,6 @@ function createServer() {
         response.writeHead(404, { 'Content-Type': 'application/json' });
         response.write(JSON.stringify({ error: 'Not found' }));
         break;
-
-      // default:
-      //   response.writeHead(200, { 'Content-Type': 'application/json' });
-      //   response.write(JSON.stringify({ state: state }));
-      //   break;
-
     }
 
     response.end();

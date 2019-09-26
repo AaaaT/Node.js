81d466ebb876aaf589a94baae6c7157b08590fb6
unknown <toegelanna@outlook.com>
2019-09-26 19:10:05 +0200

    /todos/:id

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -4,43 +4,35 @@
 
 const Express = require('express');
 const app = new Express();
-const PORT      = 3000;
+
 // Use built-in JSON middleware to automatically parse JSON
 app.use(Express.json());
 
-
    
-app.get
-    readTodo
+         // app.get-readTodo
 // readTodo (GET /todos/:id)
 // Get a single to-do with ID :id
+app.get('/todos/:id', (req, res) =>{
+    res.send(req.params.id) //e.g. http://localhost:3000/todos/1 prints 1
+})
 
-app.put
-    updateTodo
+// app.put- updateTodo
 
-app.delete
-    deleteTodo
-// clearTodos (DELETE /todos)
-// Clears the list of to-dos
+// app.delete-deleteTodo
+// // clearTodos (DELETE /todos)
+// // Clears the list of to-dos
 
-app.post
-    createTodo
-// markAsDone (POST /todos/:id/done)
-// Sets the done flag of a single to-do to true
+// app.post-createTodo
+// // markAsDone (POST /todos/:id/done)
+// // Sets the done flag of a single to-do to true
 
-app.delete
-    deleteTodo
-// markAsNotDone (DELETE /todos/:id/done)
-// Sets the done flag of a single to-do to false
+// app.delete-deleteTodo
+// // markAsNotDone (DELETE /todos/:id/done)
+// // Sets the done flag of a single to-do to false
     
 
-    
-app.listen(PORT, error => {
-    if (error)
-      return console.error(error);
-  
-    console.log(`Server started on http://localhost:${PORT}`);
-  });
+const port = process.env.PORT || 3000;  
+app.listen(port, () => console.log(`Server started listening on http://localhost:${port}`));
     
 
 

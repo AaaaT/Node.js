619b67cb3437b153b02cfd978e9dcd6c95a5634e
unknown <toegelanna@outlook.com>
2019-09-26 22:52:09 +0200

    app.get-read

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -3,6 +3,7 @@
 // TODO: Write the homework code in this file
 
 const Express = require('express');
+const fs = require('fs');
 const app = new Express();
 
 // Use built-in JSON middleware to automatically parse JSON
@@ -13,6 +14,8 @@ app.use(Express.json());
 // readTodo (GET /todos/:id)
 // Get a single to-do with ID :id
 app.get('/todos/:id', (req, res) =>{
+    let list = readList();
+    let indexId = list[]
     res.send(req.params.id) //e.g. http://localhost:3000/todos/1 prints 1
 })
 
@@ -32,7 +35,16 @@ app.post('/todos/:id/done', (req, res) =>{
 // app.delete-deleteTodo
 // // markAsNotDone (DELETE /todos/:id/done)
 // // Sets the done flag of a single to-do to false
-    
+
+function readList() {
+    let list = fs.readFileSync('list.txt');
+    return JSON.parse(list);
+  }
+
+  function writeList(list) {
+    fs.writeFileSync ('list.txt', JSON.stringify(list))
+  }
+  writeList([{text: "todo 1", done: false}, {text: "todo 2", done: false}, {text: "todo 3", done: false}, {text: "todo 4", done: false}, {text: "todo 5", done: false}]);
 
 const port = process.env.PORT || 3000;  
 app.listen(port, () => console.log(`Server started listening on http://localhost:${port}`));

283d41524d5e4b689f070a96ac489c6f728715ac
unknown <toegelanna@outlook.com>
2019-09-26 21:35:04 +0200

    Week 2 homework done

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -3,26 +3,52 @@
 // TODO: Write the homework code in this file
 const fs = require('fs');
 
-function main(()=>{
-  switch()
-    case '/list':
-  //Shows current to-dos, or shows an appropriate text if there are no to-dos
-      break;
-    case '/add':
-  // Adds a to-do item. All the words behind add are entered as 1 to-do item to the list.
-      break;
-    case '/remove':
-  // Removes a to-do item by its 1-base index, e.g. to remove second item, execute:
-      break;
-    case '/reset':
-  // Removes all to-do items from the list:
-      break;
-    default '/help':
+function main() {
+  console.log(process.argv)
+  if (process.argv.length <= 2) {
     showHelp();
+    return;
+  }
+
+  switch(process.argv[2]) {
+    case 'list':
+  //Shows current to-dos, or shows an appropriate text if there are no to-dos
+      console.log(readList());  
       break;
-  
-  });
-  main();
+    case 'add':
+  // Adds a to-do item. All the words behind add are entered as 1 to-do item to the list.
+      if (process.argv.length > 3){
+        let list = readList();
+        list.push(process.argv[3])
+        writeList(list);
+      }
+      else {
+        console.log('Could not add an item to the list.')
+      }
+      break;
+    case 'remove':
+  // Removes a to-do item by its 1-base index, e.g. to remove second item, execute:
+      if (process.argv.length > 3){
+        let list = readList();
+        list.splice(process.argv[3]-1, 1)
+        writeList(list);
+      }
+      else {
+        console.log('Could not remove the item from the list.')
+      }
+      break;
+    case 'reset':
+  // Removes all to-do items from the list:
+      writeList([]);
+      break;
+    case 'help':
+      showHelp();
+      break;
+    default:     
+      break;
+  }
+}
+main();
 
 //Functions one by one
 function showHelp() {
@@ -35,12 +61,21 @@ function showHelp() {
     }
   });
 };
-showHelp();
 
-function createTODOList() {
-  fs.writeFile('list.txt', 'List of To-Do-s', function(err) {
-    if (err) throw err;
-    console.log('List created!');
-  });
-};
-createTODOList();
+function readList() {
+  let list = fs.readFileSync('list.txt');
+  return JSON.parse(list);
+}
+
+
+// function createTODOList() {
+//   fs.writeFile('list.txt', 'List of To-Do-s', function(err) {
+//     if (err) throw err;
+//     console.log('List created!');
+//   });
+// };
+// // createTODOList();
+
+function writeList(list) {
+  fs.writeFileSync ('list.txt', JSON.stringify(list))
+}

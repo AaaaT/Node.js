421ba36bc261dff5f5e213f399bedcbf031741c0
unknown <toegelanna@outlook.com>
2019-09-30 22:46:19 +0200

    AaaaT Homework Week3 done

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,48 +1,67 @@
 'use strict';
 
-// TODO: Write the homework code in this file
-
 const Express = require('express');
 const fs = require('fs');
 const app = new Express();
-// Use built-in JSON middleware to automatically parse JSON
 app.use(Express.json());
-
-
-app.get('/todos', (req, res) =>{
-    res.send(readList()); //prints what is in todos.json
-});
-
-         // app.get-readTodo
-// readTodo (GET /todos/:id)
-// Get a single to-do with ID :id
-app.get('/todos/:id', (req, res) =>{
-    res.send(req.param.id); //e.g. http://localhost:3000/todos/1 prints 1
-    
-})
-
-// app.put- updateTodo
-
-// app.delete-deleteTodo
-// // clearTodos (DELETE /todos)
-// // Clears the list of to-dos
-
-// app.post-createTodo
-// // markAsDone (POST /todos/:id/done)
-// // Sets the done flag of a single to-do to true
-app.post('/todos/:id/done', (req, res) =>{
-    
-})
-
-// app.delete-deleteTodo
-// // markAsNotDone (DELETE /todos/:id/done)
-// // Sets the done flag of a single to-do to false
 
 
 function readList() {
   let list = fs.readFileSync('todos.json');
-  return list;
-}
+  return JSON.parse(list);
+};
+
+function writeList(list) {
+  fs.writeFileSync ('todos.json', JSON.stringify(list))
+};
+
+
+
+app.get('/todos', (req, res) =>{
+    res.send(readList());
+});
+
+
+app.get('/todos/:id', (req, res) =>{
+  const todos = readList();
+  let myTodo = "";
+  for ( const todo of todos) {
+    if (req.params.id == todo.id) {
+      myTodo = JSON.stringify(todo)
+    }
+  }
+  res.send(myTodo)  
+});
+
+
+app.delete ('/todos', (req, res) =>{
+  writeList([]);
+  res.send();
+});
+
+
+app.put('/todos/:id/done', (req, res) =>{
+  const todos = readList();
+  for ( let todo of todos) {
+    if (req.params.id == todo.id) {
+      todo.done = true
+      writeList(todos)
+    }
+  }
+  res.send() 
+});
+
+app.put('/todos/:id/notdone', (req, res) =>{
+  const todos = readList();
+  for ( let todo of todos) {
+    if (req.params.id == todo.id) {
+      todo.done = false
+      writeList(todos)
+    }
+  }
+  res.send() 
+});
+
 
 const port = process.env.PORT || 3000;  
 app.listen(port, () => console.log(`Server started listening on http://localhost:${port}`));
